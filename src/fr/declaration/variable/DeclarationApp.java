afa3bcea82cdee5c1a96a4f4c8813c4ef34e87ed
Nathan <znathangirotz@gmail.com>
2020-06-23 13:59:15 +0200

    first commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Salut c'est moi T'choupi");
+		byte b = 2;
+		short s = 1;
+		int i = 5;
+		long l = 2347248L;
+		float f = 2.8f;
+		char c = '@';
+		boolean bool = true;
+		String st = "Toudou bem";
+		System.out.println(bool);
+		String randomString = "Voici le résultat d’un calcul :\n1+5=6";
+		System.out.println(randomString);
+	}
+
+}
